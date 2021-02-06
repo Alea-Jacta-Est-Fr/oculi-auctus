6649938119cc8fe0168df00f004e102aa78b68fb
Thibault PETIT <thibault.petit@hotmail.fr>
2021-02-06 19:58:21 +0100

    Update Marker + Button + mirror on large screen

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ document.addEventListener("keypress", moveTarget);
 
 function moveTarget(){
 
-rayon = 2;
-
-newX = Math.random(rayon)-rayon/2;
-newZ = Math.random(rayon)-rayon/2;
+newX = 0;
+newZ = 0;
 
 // document.getElementById('mon-plan').position = newX+" "+0+" "+newZ;
-document.getElementById('movingTarget').setAttribute('position', {x: newX,  y: 0.15,  z: newZ});
+document.getElementById('movingTarget').setAttribute(
+	'position', 
+	{x: newX,  y: 0.15,  z: newZ}
+);
 
 }
 
