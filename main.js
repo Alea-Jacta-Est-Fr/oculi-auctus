cb48e39e7b7b81ecf814b83b2a1a79b6b975ddc8
Thibault PETIT <thibault.petit@hotmail.fr>
2021-02-07 12:44:12 +0100

    Update marker + clean repo

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("keypress", moveTarget);
 
-
+// init();
 /*
 Pourquoi pas une function qui permettrait de placer ne manière "aléatoire" les 
 targets du plateau ?
@@ -22,4 +22,24 @@ document.getElementById('movingTarget').setAttribute(
 
 }
 
+// function init(){
+
+// 	arrayOfMarker = [
+// 	'pattern-x0',
+// 	'pattern-x1',
+// 	'pattern-x2',
+// 	'pattern-x5',
+// 	'pattern-x10',
+// 	'pattern-joker'
+// 	];
+	
+// 	htmlToAdd = '';
+
+// 	for (var i = arrayOfMarker.length - 1; i >= 0; i--) {
+// 		htmlToAdd += '<a-marker type="pattern" url="ressources/patterns/'+arrayOfMarker[i]+'.patt"><a-plane position="0 0.15 0" rotation="-90 0 0" scale="1 1 1" material="src:#target; transparent: true; opacity:1"></a-plane></a-marker>';
+// 	}
+
+// 	document.getElementById('main-scene').innerHTML += htmlToAdd;
+
+// }
 
